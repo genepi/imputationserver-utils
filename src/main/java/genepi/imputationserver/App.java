ff1be4d59b59dcfec952a5a22b000024c1e2dc87
seppinho <sebastian.schoenherr@i-med.ac.at>
2023-09-11 10:02:37 +0200

    Prepare releave v1.2.0

--- src/main/java/genepi/imputationserver/App.java
+++ src/main/java/genepi/imputationserver/App.java
@@ -13,7 +13,7 @@ public class App {
 
 	public static final String APP = "imputationserver-utils";
 
-	public static final String VERSION = "1.1.3";
+	public static final String VERSION = "1.2.0";
 
 	public static final String URL = "https://github.com/genepi/imputationserver-utils";
 
