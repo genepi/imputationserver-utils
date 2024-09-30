ac3d37376829eef8feea74cb09ebf4a0f3db52be
Lukas Forer <lukas.forer@i-med.ac.at>
2024-09-30 14:49:48 +0200

    Prepare release 1.5.0

--- src/main/java/genepi/imputationserver/App.java
+++ src/main/java/genepi/imputationserver/App.java
@@ -12,7 +12,7 @@ public class App {
 
 	public static final String APP = "imputationserver-utils";
 
-	public static final String VERSION = "1.4.5";
+	public static final String VERSION = "1.5.0";
 
 	public static final String URL = "https://github.com/genepi/imputationserver-utils";
 
