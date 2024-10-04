d3403f99440a04bf6fd097d2f378a99d555536f1
Sebastian Schönherr <seb.schoenherr@gmail.com>
2024-10-04 19:32:25 +0200

    Prepare release v1.5.1

--- src/main/java/genepi/imputationserver/App.java
+++ src/main/java/genepi/imputationserver/App.java
@@ -12,7 +12,7 @@ public class App {
 
 	public static final String APP = "imputationserver-utils";
 
-	public static final String VERSION = "1.5.0";
+	public static final String VERSION = "1.5.1";
 
 	public static final String URL = "https://github.com/genepi/imputationserver-utils";
 
